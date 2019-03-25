ead40007177565d03ab3f22e3a793d7b75c56114
Rafael Piloto <31188361+RafaelPiloto10@users.noreply.github.com>
2019-03-24 22:13:52 -0400

    added displaying the game pieces, more scouting options

--- src/public/field.js
+++ src/public/field.js
@@ -1,8 +1,8 @@
 class Field {
     constructor() {
         this.cargo_ship = new CargoShip();
-        this.rocketF = new Rocket();
-        this.rocketN = new Rocket();
+        this.rocketF = new Rocket(false);
+        this.rocketN = new Rocket(true);
     }
 }
 
@@ -29,6 +29,16 @@ class CargoShip {
             [false, false],
             [false, false]
         ]
+        this.shipPoints = [
+            [createVector(100 + 70 + 64, 190 + 70), createVector(100 + 70, 190 + 70)],
+            [createVector(100 + 70 + 64, 395 + 50), createVector(100 + 70, 395 + 50)],
+            [createVector(285 + 70 + 64, 190 + 70), createVector(285 + 70, 190 + 70)],
+            [createVector(285 + 70 + 64, 285 + 140), createVector(285 + 70, 285 + 140)],
+            [createVector(470 + 50 + 64, 190 + 70), createVector(470 + 50, 190 + 70)],
+            [createVector(470 + 50 + 64, 285 + 140), createVector(470 + 50, 285 + 140)],
+            [createVector(600 + 70 + 64, 190 + 70), createVector(600 + 70, 190 + 70)],
+            [createVector(600 + 70 + 64, 285 + 140), createVector(600 + 70, 285 + 140)]
+        ];
     }
 
     toggle(location, piece) {
@@ -58,23 +68,86 @@ class CargoShip {
                 this.ship[7][piece] = !this.ship[7][piece];
                 break;
         }
+
+        console.log(this.ship);
     }
 
-    display() {}
+    display() {
+        for (let level = 0; level < this.ship.length; level++) {
+            for (let piece = 0; piece < this.ship[level].length; piece++) {
+                if (this.ship[level][piece]) {
+                    if (piece == 1) {
+                        fill(232, 221, 25);
+                        ellipse(this.shipPoints[level][piece].x, this.shipPoints[level][piece].y, 64);
+                    } else {
+                        fill(244, 154, 9);
+                        ellipse(this.shipPoints[level][piece].x, this.shipPoints[level][piece].y, 64);
+                    }
+                }
+            }
+        }
+    }
 }
 
 class Rocket {
-    constructor() {
+    constructor(isNear) {
         /*
-        Hatch   Cargo   Hatch
-        [false, false, false] - LEVEL 1
-        [false, false, false] - LEVEL 2
-        [false, false, false] - LEVEL 3
+        Hatch   Cargo  Cargo  Hatch
+        [false, false, false, false] - LEVEL 1
+        [false, false, false, false] - LEVEL 2
+        [false, false, false, false] - LEVEL 3
          */
         this.rocket = [
-            [false, false, false],
-            [false, false, false],
-            [false, false, false],
-        ]
+            [false, false, false, false],
+            [false, false, false, false],
+            [false, false, false, false],
+        ];
+
+        this.rocketFPoints = [
+            [createVector(916, 240 + 16), createVector(935 + 16, 240 + 30), createVector(935 + 36, 240 + 30), createVector(1016, 240 + 16)],
+            [createVector(916, 160 + 16), createVector(935 + 16, 150 + 30), createVector(935 + 36, 150 + 30), createVector(1016, 160 + 16)],
+            [createVector(916, 60 + 16), createVector(935 + 16, 45 + 30), createVector(935 + 36, 45 + 30), createVector(1016, 60 + 16)]
+        ];
+
+        this.rocketNPoints = [
+            [createVector(917, 570 + 16), createVector(935 + 16, 565 + 30), createVector(935 + 45, 565 + 30), createVector(1017, 570 + 16)],
+            [createVector(917, 485 + 16), createVector(935 + 16, 475 + 30), createVector(935 + 45, 475 + 30), createVector(1017, 485 + 16)],
+            [createVector(917, 395 + 16), createVector(935 + 16, 370 + 30), createVector(935 + 45, 370 + 30), createVector(1017, 395 + 16)]
+        ];
+
+        this.isNear = isNear;
+    }
+    toggle(location, piece) {
+        if ((piece == 0 || piece == 3) && document.getElementById("gamepieceB").innerHTML == "Hatch") {
+            this.rocket[location - 1][piece] = !this.rocket[location - 1][piece]
+        } else if ((piece == 1 || piece == 2) && document.getElementById("gamepieceB").innerHTML == "Cargo") {
+            this.rocket[location - 1][piece] = !this.rocket[location - 1][piece]
+        }
+    }
+
+    display() {
+        for (let level = 0; level < this.rocket.length; level++) {
+            for (let piece = 0; piece < this.rocket[level].length; piece++) {
+                if (piece != 1 && piece != 2) {
+                    fill(232, 221, 25);
+                    if (this.rocket[level][piece]) {
+                        if (this.isNear) {
+                            ellipse(this.rocketNPoints[level][piece].x, this.rocketNPoints[level][piece].y, 16);
+                        } else {
+                            ellipse(this.rocketFPoints[level][piece].x, this.rocketFPoints[level][piece].y, 16);
+                        }
+                    }
+                } else {
+                    fill(244, 154, 9);
+                    if (this.rocket[level][piece]) {
+                        if (this.isNear) {
+                            ellipse(this.rocketNPoints[level][piece].x, this.rocketNPoints[level][piece].y, 16);
+                        } else {
+                            ellipse(this.rocketFPoints[level][piece].x, this.rocketFPoints[level][piece].y, 16);
+                        }
+                    }
+                }
+            }
+        }
     }
 }
--- src/public/sketch.js
+++ src/public/sketch.js
@@ -22,89 +22,142 @@ function draw() {
     }
     ellipse(mouseX, mouseY, 16);
 
+    field.cargo_ship.display();
+    field.rocketF.display();
+    field.rocketN.display();
+
 }
 
 function mouseClicked() {
     console.log(mouseX, mouseY);
     if (mouseY > 0) {
+        let p = (document.getElementById("gamepieceB").innerHTML == "Hatch") ? 1 : 0;
         if (mouseX >= 100 && mouseX <= 285) {
             // Front cargo ship
             if (mouseY >= 190 && mouseY <= 395) {
-                console.log("cargo F front");
+                console.log("ship F front");
+                field.cargo_ship.toggle("front-F", p);
             } else if (mouseY > 395 && mouseY < 600) {
-                console.log("cargo N front");
+                console.log("ship N front");
+                field.cargo_ship.toggle("front-N", p);
             }
 
         } else if (mouseX > 285 && mouseX <= 470) {
             // Cargo ship 1
             if (mouseY >= 190 && mouseY <= 385) {
                 console.log("ship F 1");
+                field.cargo_ship.toggle("F1", p);
             } else if (mouseY > 385 && mouseY <= 600) {
                 console.log("ship N 1");
+                field.cargo_ship.toggle("N1", p);
             }
 
         } else if (mouseX > 470 && mouseX <= 630) {
             // Cargo ship 2
             if (mouseY >= 190 && mouseY <= 385) {
                 console.log("ship F 2");
+                field.cargo_ship.toggle("F2", p);
             } else if (mouseY > 385 && mouseY <= 600) {
                 console.log("ship N 2");
+                field.cargo_ship.toggle("N2", p);
             }
 
         } else if (mouseX > 600 && mouseX <= 805) {
             // Cargo ship 3
             if (mouseY >= 190 && mouseY <= 385) {
                 console.log("ship F 3");
+                field.cargo_ship.toggle("F3", p);
             } else if (mouseY > 385 && mouseY <= 600) {
                 console.log("ship N 3");
+                field.cargo_ship.toggle("N3", p);
             }
 
         } else if (mouseX >= 900 && mouseX <= 935) {
             // Rocket hatch
             if (mouseY >= 60 && mouseY <= 140) {
                 console.log("Rocket F 3 HATCH");
+                field.rocketF.toggle(3, 0);
             } else if (mouseY >= 160 && mouseY <= 220) {
                 console.log("Rocket F 2 HATCH");
+                field.rocketF.toggle(2, 0);
             } else if (mouseY >= 240 && mouseY <= 305) {
                 console.log("Rocket F 1 HATCH");
+                field.rocketF.toggle(1, 0);
             } else if (mouseY >= 395 && mouseY <= 470) {
                 console.log("Rocket N 3 HATCH");
+                field.rocketN.toggle(3, 0);
             } else if (mouseY >= 485 && mouseY <= 555) {
                 console.log("Rocket N 2 HATCH");
+                field.rocketN.toggle(2, 0);
             } else if (mouseY >= 570 && mouseY <= 635) {
                 console.log("Rocket N 1 HATCH");
+                field.rocketN.toggle(1, 0);
             }
 
-        } else if (mouseX > 935 && mouseX <= 1000) {
-            // Rocket cargo
+        } else if (mouseX > 935 && mouseX <= 960) {
+            // Rocket cargo left
             if (mouseY >= 45 && mouseY <= 105) {
                 console.log("Rocket F 3 CARGO")
+                field.rocketF.toggle(3, 1);
             } else if (mouseY >= 150 && mouseY <= 200) {
                 console.log("Rocket F 2 CARGO");
+                field.rocketF.toggle(2, 1);
             } else if (mouseY >= 240 && mouseY <= 290) {
                 console.log("Rocket F 1 CARGO");
+                field.rocketF.toggle(1, 1);
             } else if (mouseY >= 370 && mouseY <= 430) {
                 console.log("Rocket N 3 CARGO");
+                field.rocketN.toggle(3, 1);
             } else if (mouseY >= 475 && mouseY <= 530) {
                 console.log("Rocket N 2 CARGO");
+                field.rocketN.toggle(2, 1);
             } else if (mouseY >= 565 && mouseY <= 615) {
                 console.log("Rocket N 1 CARGO");
+                field.rocketN.toggle(1, 1);
+            }
+
+        } else if (mouseX > 960 && mouseX <= 1000) {
+            // Rocket cargo right
+            if (mouseY >= 45 && mouseY <= 105) {
+                console.log("Rocket F 3 CARGO")
+                field.rocketF.toggle(3, 2);
+            } else if (mouseY >= 150 && mouseY <= 200) {
+                console.log("Rocket F 2 CARGO");
+                field.rocketF.toggle(2, 2);
+            } else if (mouseY >= 240 && mouseY <= 290) {
+                console.log("Rocket F 1 CARGO");
+                field.rocketF.toggle(1, 2);
+            } else if (mouseY >= 370 && mouseY <= 430) {
+                console.log("Rocket N 3 CARGO");
+                field.rocketN.toggle(3, 2);
+            } else if (mouseY >= 475 && mouseY <= 530) {
+                console.log("Rocket N 2 CARGO");
+                field.rocketN.toggle(2, 2);
+            } else if (mouseY >= 565 && mouseY <= 615) {
+                console.log("Rocket N 1 CARGO");
+                field.rocketN.toggle(1, 2);
             }
 
         } else if (mouseX > 1000 && mouseX <= 1030) {
             // Rocket hatch
             if (mouseY >= 60 && mouseY <= 140) {
                 console.log("Rocket F 3 HATCH");
+                field.rocketF.toggle(3, 3);
             } else if (mouseY >= 160 && mouseY <= 220) {
                 console.log("Rocket F 2 HATCH");
+                field.rocketF.toggle(2, 3);
             } else if (mouseY >= 240 && mouseY <= 305) {
                 console.log("Rocket F 1 HATCH");
+                field.rocketF.toggle(1, 3);
             } else if (mouseY >= 395 && mouseY <= 470) {
                 console.log("Rocket N 3 HATCH");
+                field.rocketN.toggle(3, 3);
             } else if (mouseY >= 485 && mouseY <= 555) {
                 console.log("Rocket N 2 HATCH");
+                field.rocketN.toggle(2, 3);
             } else if (mouseY >= 570 && mouseY <= 635) {
                 console.log("Rocket N 1 HATCH");
+                field.rocketN.toggle(1, 3);
             }
         }
     }
